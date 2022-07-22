17535f01aae46e8fcc6a1cb1c676c6ff3d0203b0
Joel Odhiambo <wesleyjoel4@gmail.com>
2022-07-22 14:38:01 +0300

    fixed encryption and decryption

--- ciphers/aes.py
+++ ciphers/aes.py
@@ -1,75 +1,74 @@
 import os
-from hashlib import md5
+
 from Cryptodome.Cipher import AES
-from Cryptodome import Random
-from Cryptodome.Random import get_random_bytes
 from Cryptodome.Protocol.KDF import scrypt
-import os
+from Cryptodome.Random import get_random_bytes
 
 BUFFER_SIZE = 1024 * 1024
 
+
 class AesCipher:
-    def __init__(self,in_filename, password):
-        
-        self.in_filename=in_filename
-        self.out_filename=self.in_filename + '.encrypted'
-        
-        self.file_in=open(self.in_filename,'rb')
-        self.password=password
-        
+    def __init__(self, in_filename, password):
+
+        self.in_filename = in_filename
+        self.out_filename = self.in_filename + '.encrypted'
+
+        self.file_in = open(self.in_filename, 'rb')
+        self.password = password
+
     def encrypt_file(self):
-        
-        save_path='C:\Krypter\Files\encrypted files\AES'
-        file_path=os.path.join(save_path,os.path.basename(self.out_filename))
-        file_out= open(file_path,'wb')
-        
+
+        save_path = 'C:/Krypter/Files/encrypted files/AES'
+        encrypted_file_path = os.path.join(save_path, os.path.basename(self.out_filename))
+        file_out = open(encrypted_file_path, 'wb')
+
         salt = get_random_bytes(32)
-        key = scrypt(self.password, salt, key_len=32, N=2**20, r=8, p=1)
+        key = scrypt(self.password, salt, key_len=32, N=2 ** 20, r=8, p=1)
         file_out.write(salt)
         cipher = AES.new(key, AES.MODE_GCM)
         file_out.write(cipher.nonce)
-        
-        plain_text=self.file_in.read(BUFFER_SIZE)
-        
-        while len(plain_text)!=0:
-            cipher_text=cipher.encrypt(plain_text)
-            file_out.write(cipher_text)
-            plain_text=self.file_in.read(BUFFER_SIZE)
 
-        tag=cipher.digest()
+        plain_text = self.file_in.read(BUFFER_SIZE)
+
+        while len(plain_text) != 0:
+            cipher_text = cipher.encrypt(plain_text)
+            file_out.write(cipher_text)
+            plain_text = self.file_in.read(BUFFER_SIZE)
+
+        tag = cipher.digest()
         file_out.write(tag)
         file_out.close()
-        
-    def decrypt_file(self,file_in, file_out, password):
-        size_of_file_in=os.path.getsize(file_in)
-        file_out_path=file_out
+
+    def decrypt_file(self):
+        save_path = 'C:/Krypter/Files/decrypted files/AES'
+        decrypted_file_path = os.path.join(save_path, os.path.basename(self.file_in.name).removesuffix('.encrypted'))
+
+        size_of_file_in = os.path.getsize(self.file_in.name)
         print(size_of_file_in)
-        file_in = open(file_in,'rb')
-        file_out= open(file_out,'wb')
-        
+        file_out = open(decrypted_file_path, 'wb')
+
         block_size = AES.block_size
-        salt = file_in.read(32)
+        salt = self.file_in.read(32)
         print(salt)
-        key = scrypt(password, salt, key_len=32, N=2**20, r=8, p=1)
-        nonce=file_in.read(block_size)
+        key = scrypt(self.password, salt, key_len=32, N=2 ** 20, r=8, p=1)
+        nonce = self.file_in.read(block_size)
         cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
-        
-        size_of_cipher_text=size_of_file_in-32-16-16
-        
+
+        size_of_cipher_text = size_of_file_in - 32 - 16 - 16
+
         for _ in range(int(size_of_cipher_text / BUFFER_SIZE)):
-            data=file_in.read(BUFFER_SIZE)
-            plain_text=cipher.decrypt(data)
+            data = self.file_in.read(BUFFER_SIZE)
+            plain_text = cipher.decrypt(data)
             file_out.write(plain_text)
-        data =file_in.read(int(size_of_cipher_text % BUFFER_SIZE))
-        plain_text=cipher.decrypt(data)
+        data = self.file_in.read(int(size_of_cipher_text % BUFFER_SIZE))
+        plain_text = cipher.decrypt(data)
         file_out.write(plain_text)
-        
-        tag=file_in.read(16)
+
+        tag = self.file_in.read(16)
         try:
             cipher.verify(tag)
         except ValueError as e:
-            file_in.close()
+            self.file_in.close()
             file_out.close()
-            os.remove(file_out_path)
+            os.remove(decrypted_file_path)
             raise e
-            
--- krypter.py
+++ krypter.py
@@ -2,14 +2,11 @@ import os
 import shutil
 from functools import partial
 import tkinter as tk
-from tkinter import  DISABLED, Label, Menu, PhotoImage,StringVar, ttk
+from tkinter import DISABLED, Frame, Label, Menu, PhotoImage, StringVar, ttk
 from tkinter import filedialog
 
 import random
 import string
-
-
-
 
 from ciphers.aes import AesCipher as aes_cipher
 from ciphers.twofish_cipher import TwofishCipher as two_fish
@@ -22,13 +19,14 @@ class KrypterApp(tk.Tk):
         super().__init__()
 
         self.app_dir = 'C:/Krypter/Files'
-        
-        list=('original files','encrypted files/AES','encrypted files/Two fish')
 
-        app_path=partial(os.path.join,self.app_dir)
-        make_directory=partial(os.makedirs, exist_ok=True)
-        
-        for directories in map(app_path,list):
+        list = ('original files', 'encrypted files/AES',
+                'encrypted files/Two fish')
+
+        app_path = partial(os.path.join, self.app_dir)
+        make_directory = partial(os.makedirs, exist_ok=True)
+
+        for directories in map(app_path, list):
             make_directory(directories)
 
         self.title('Krypter')
@@ -40,119 +38,240 @@ class KrypterApp(tk.Tk):
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=3)
 
-        self.menu_bar = Menu(self)
-        self.config(menu=self.menu_bar)
-        self.file_menu = Menu(self.menu_bar, tearoff=0)
-        self.file_menu.add_command(
-            label="Open",
-            command=self.open_file
-        )
+        # self.menu_bar = Menu(self)
 
-        self.file_menu.add_separator()
-        self.sub_menu = Menu(self.file_menu, tearoff=0)
-        self.sub_menu.add_command(label='Toggle Dark Mode')
-        self.file_menu.add_cascade(
-            label="Preferences",
-            menu=self.sub_menu
-        )
-        self.file_menu.add_separator()
+        # self.config(menu=self.menu_bar)
 
-        self.file_menu.add_command(
-            label="Quit",
-            command=self.destroy
-        )
+        # self.file_menu = Menu(self.menu_bar, tearoff=0)
+        # self.file_menu.add_command(
+        #     label="Open",
+        #     command=self.open_file
+        # )
 
-        self.menu_bar.add_cascade(
-            label="File",
-            menu=self.file_menu,
-            underline=0
-        )
+        # self.file_menu.add_separator()
 
-        self.label_one = Label(self, text="1. Choose file to encrypt")
-        self.label_one.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+        # self.sub_menu = Menu(self.file_menu, tearoff=0)
 
-        self.path_name = StringVar(None)
+        # self.sub_menu.add_command(label='Toggle Dark Mode')
+        # self.file_menu.add_cascade(
+        #     label="Preferences",
+        #     menu=self.sub_menu
+        # )
 
-        self.file_path_box = ttk.Entry(
-            self, textvariable=self.path_name, width=44)
-        self.file_path_box.grid(column=0, row=1, sticky=tk.W, padx=20, pady=5)
+        # self.file_menu.add_separator()
 
-        self.choose_button = ttk.Button(
-            text="Choose File", command=self.open_file)
-        self.choose_button.grid(column=1, row=1, sticky=tk.W, pady=5)
+        # self.file_menu.add_command(
+        #     label="Quit",
+        #     command=self.destroy
+        # )
+
+        # self.menu_bar.add_cascade(
+        #     label="File",
+        #     menu=self.file_menu,
+        #     underline=0
+        # )
+
+        # self.encrypt_menu = Menu(self.menu_bar, tearoff=0)
+
+        # self.menu_bar.add_cascade(
+        #     label="Encrypt",
+        #     menu=self.encrypt_menu,
+        #     command=self.change_to_encrypt
+
+        # )
+
+        # self.decrypt_menu = Menu(self.menu_bar, tearoff=0)
+
+        # self.menu_bar.add_cascade(
+        #     label="Decrypt",
+        #     menu=self.decrypt_menu,
+        #     command=self.change_to_decrypt
+        # )
+
+        self.notebook = ttk.Notebook(self)
+
+        self.encrypt_frame = Frame(self.notebook)
+
+        self.decrypt_frame = Frame(self.notebook)
+
+        self.notebook.pack(expand=1, fill="both")
+
+        self.notebook.add(self.encrypt_frame, text="Encrypt")
+        self.notebook.add(self.decrypt_frame, text="Decrypt")
+
+        self.label_choose = Label(self.encrypt_frame, text="1. Choose file to encrypt")
+        self.label_choose.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+
+        # File to encrypt Path Variable
+        self.original_file_path = StringVar(None)
+
+        self.original_file_entry = ttk.Entry(self.encrypt_frame, width=44)
+        self.original_file_entry.grid(column=0, row=1, sticky=tk.W, padx=20, pady=5)
+
+        self.bool_one = StringVar(None)
+        self.choose_button_one = ttk.Button(self.encrypt_frame,
+                                            text="Choose File", name="btn_one", command=self.open_original_file)
+        self.choose_button_one.grid(column=1, row=1, sticky=tk.W, pady=5)
 
         self.label_two = Label(
-            self, text="2. Enter a password and choose an Encryption Algorithm")
+            self.encrypt_frame, text="2. Enter a password and choose an Encryption Algorithm")
         self.label_two.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
 
         self.default_password_var = tk.StringVar(None)
 
         self.password_entry = ttk.Entry(
-            self, textvariable=self.default_password_var, width=44)
+            self.encrypt_frame, textvariable=self.default_password_var, width=44)
         self.password_entry.grid(column=0, row=3, sticky=tk.W, padx=20, pady=5)
 
-        self.generate_button = ttk.Button(
-            text="Generate Password", command=self.password_generator)
+        self.generate_button = ttk.Button(self.encrypt_frame,
+                                          text="Generate Password", command=self.password_generator)
         self.generate_button.grid(column=1, row=3, sticky=tk.W, pady=5)
 
         self.check_button = ttk.Checkbutton(
-            self, text="Hide Password", onvalue=False, offvalue=True, command=self.toggle_password)
+            self.encrypt_frame, text="Hide Password", onvalue=False, offvalue=True, command=self.toggle_password)
 
         self.check_button.grid(column=0, row=4, sticky=tk.W, padx=100)
 
         self.radio_var = StringVar(None)
-        self.aes_radio_button = ttk.Radiobutton(text='AES',
-                                                variable=self.radio_var,
-                                                value=1)
-        self.aes_radio_button.grid(column=0, row=5, sticky=tk.W, padx=19)
-        self.two_fish_radio_button = ttk.Radiobutton(text='Two Fish',
-                                                     variable=self.radio_var,
-                                                     value=2)
-        self.two_fish_radio_button.grid(column=0, row=6, sticky=tk.W, padx=19)
+        self.aes_radio_button_e = ttk.Radiobutton(self.encrypt_frame,
+                                                  text='AES',
+                                                  variable=self.radio_var,
+                                                  value=1)
+        self.aes_radio_button_e.grid(column=0, row=5, sticky=tk.W, padx=19)
+        self.two_fish_radio_button_e = ttk.Radiobutton(self.encrypt_frame,
+                                                       text='Two Fish',
+                                                       variable=self.radio_var,
+                                                       value=2)
+        self.two_fish_radio_button_e.grid(column=0, row=6, sticky=tk.W, padx=19)
 
         self.label_three = Label(
-            self, text="3. Encryption")
+            self.encrypt_frame, text="3. Encryption")
         self.label_three.grid(column=0, row=7, sticky=tk.W, padx=5, pady=10)
 
         self.label_four = Label(
-            self, text="Working Directory: ")
+            self.encrypt_frame, text="Working Directory: ")
         self.label_four.grid(column=0, row=8, sticky=tk.W, padx=17, pady=5)
 
-        self.app_directory_var = StringVar(None)
-        
-        
-        self.app_directory_entry = ttk.Entry(
-            self, textvariable=self.app_directory_var, width=44)
-        
-        self.app_directory_entry.config(state=DISABLED)
-        self.app_directory_entry.place(x=130,y=242)
+        self.original_file_var = StringVar(None)
+
+        self.encrypted_file_dest = ttk.Entry(
+            self.encrypt_frame, width=44)
+
+        self.encrypted_file_dest.config(state=DISABLED)
+        self.encrypted_file_dest.place(x=130, y=242)
 
         self.encryption_photo = PhotoImage(
             file=r"icons\wicons8-password-50.png")
         self.encryption_photo_image = self.encryption_photo.subsample(3, 3)
 
-        self.encrypt_button = ttk.Button(
-            text="Encrypt", command=self.selected)
+        self.encrypt_button = ttk.Button(self.encrypt_frame,
+                                         text="Encrypt", command=self.selected)
         self.encrypt_button.grid(column=0, row=9, sticky=tk.W, padx=19, pady=5)
 
-
-        self.separator_two=ttk.Separator(self,orient='horizontal')
-        self.separator_two.place(relx=0,rely=0.95,relheight=0.5,relwidth=1)
+        # self.separator_two = ttk.Separator(self, orient='horizontal')
+        # self.separator_two.place(relx=0, rely=0.95, relheight=0.5, relwidth=1)
 
         # self.progress_bar=ttk.Progressbar(self,orient='horizontal',mode='indeterminate',length=100)
         # self.progress_bar.place(x=1,y=453)
-        
-    def open_file(self):
-        
-        self.file_types = (("Text files", "*.txt*"), ("All files", "*.*"))
-        self.selected_file = filedialog.askopenfilename(initialdir="/",
-                                                    title="Choose a File",
-                                                    filetypes=(("Text files", "*.txt*"), ("All files", "*.*")))
-        self.path_name.set(self.selected_file)
-        shutil.copy(self.selected_file,'C:\Krypter\Files\original files')
-        self.file_name='C:\Krypter\Files\original files\\'+os.path.basename(self.selected_file)
-        self.app_directory_var.set(self.file_name)
-        print( self.file_name)
+
+        self.label_choose_d = Label(self.decrypt_frame, text="1. Choose file to decrypt")
+        self.label_choose_d.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+
+        # File to decrypt path variable
+        self.encrypted_file_path = StringVar(None)
+
+        self.encrypted_file_entry = ttk.Entry(
+            self.decrypt_frame, width=44)
+        self.encrypted_file_entry.grid(column=0, row=1, sticky=tk.W, padx=20, pady=5)
+
+        self.bool_two = StringVar(None)
+        self.choose_button_two = ttk.Button(self.decrypt_frame,
+                                            text="Choose File ", name="btn_two", command=self.open_encrypted_file)
+        self.choose_button_two.grid(column=1, row=1, sticky=tk.W, pady=5)
+
+        self.label_two = Label(
+            self.decrypt_frame, text="2. Enter your password and choose an the Encryption Algorithm")
+        self.label_two.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
+
+        self.default_password_var = tk.StringVar(None)
+
+        self.password_entry = ttk.Entry(
+            self.decrypt_frame, textvariable=self.default_password_var, width=44)
+        self.password_entry.grid(column=0, row=3, sticky=tk.W, padx=20, pady=5)
+
+        self.check_button = ttk.Checkbutton(
+            self.decrypt_frame, text="Hide Password", onvalue=False, offvalue=True, command=self.toggle_password)
+
+        self.check_button.grid(column=0, row=4, sticky=tk.W, padx=100)
+
+        self.radio_var_two = StringVar(None)
+        self.aes_radio_button_d = ttk.Radiobutton(self.decrypt_frame,
+                                                  text='AES',
+                                                  variable=self.radio_var_two,
+                                                  value=3)
+        self.aes_radio_button_d.grid(column=0, row=5, sticky=tk.W, padx=19)
+        self.two_fish_radio_button_d = ttk.Radiobutton(self.decrypt_frame,
+                                                       text='Two Fish',
+                                                       variable=self.radio_var_two,
+                                                       value=4)
+        self.two_fish_radio_button_d.grid(column=0, row=6, sticky=tk.W, padx=19)
+
+        self.label_three = Label(
+            self.decrypt_frame, text="3. Decryption")
+        self.label_three.grid(column=0, row=7, sticky=tk.W, padx=5, pady=10)
+
+        self.label_five = Label(
+            self.decrypt_frame, text="Working Directory: ")
+        self.label_five.grid(column=0, row=8, sticky=tk.W, padx=17, pady=5)
+
+        self.encrypted_file_var = StringVar(None)
+
+        self.encrypted_file_path = ttk.Entry(
+            self.decrypt_frame, textvariable=self.encrypted_file_var, width=44)
+
+        self.encrypted_file_path.config(state=DISABLED)
+        self.encrypted_file_path.place(x=130, y=238)
+
+        self.encryption_photo = PhotoImage(
+            file=r"icons\wicons8-password-50.png")
+        self.encryption_photo_image = self.encryption_photo.subsample(3, 3)
+
+        self.decrypt_button = ttk.Button(self.decrypt_frame,
+                                         text="Decrypt", command=self.selected)
+        self.decrypt_button.grid(column=0, row=9, sticky=tk.W, padx=19, pady=5)
+
+    def open_original_file(self):
+
+        self.file_types = (("All files", "*.*"), ("Text files", "*.txt*"))
+        self.original_file = filedialog.askopenfilename(initialdir="/",
+                                                        title="Choose a File",
+                                                        filetypes=(("All files", "*.*"), ("Text files", "*.txt*")))
+
+        self.original_file_path.set(self.original_file)
+        self.original_file_entry.config(textvariable=self.original_file_path)
+
+        shutil.copy(self.original_file, 'C:\Krypter\Files\original files')
+        self.file_name = 'C:\Krypter\Files\original files\\' + \
+                         os.path.basename(self.original_file)
+        self.original_file_var.set(self.file_name)
+        self.encrypted_file_dest.config(textvariable=self.original_file_var)
+        # if self.choose_button_one._name=="btn_one":
+        #     self.original_file_var.set(self.file_name)
+        # if self.choose_button_two._name=="btn_two":    
+        #     self.encrypted_file_var.set(self.file_name)
+        # print(self.file_name)
+
+    def open_encrypted_file(self):
+        self.file_types = (("All files", "*.*"), ("Text files", "*.txt*"))
+        self.encrypted_file = filedialog.askopenfilename(initialdir="/",
+                                                         title="Choose a File",
+                                                         filetypes=(("All files", "*.*"), ("Text files", "*.txt*")))
+        self.encrypted_file_var.set(self.encrypted_file)
+        self.encrypted_file_entry.config(textvariable=self.encrypted_file_var)
+
+        # shutil.copy(self.encrypted_file, 'C:\Krypter\Files\original files')
+        # self.file_name = 'C:\Krypter\Files\original files\\' + \
+        #                  os.path.basename(self.encrypted_file)
 
     def toggle_password(self):
         if self.password_entry.cget('show') == '':
@@ -167,7 +286,7 @@ class KrypterApp(tk.Tk):
         self.symbols = string.punctuation
 
         self.all_characters = self.lowercase_characters + \
-            self.uppercase_characters + self.digits + self.symbols
+                              self.uppercase_characters + self.digits + self.symbols
 
         self.password_combo = random.sample(
             self.all_characters, MAX_PASSWORD_LENGTH)
@@ -176,15 +295,32 @@ class KrypterApp(tk.Tk):
         self.default_password_var.set(self.default_password)
 
     def selected(self):
-       
-        if self.radio_var.get() =='1':
-            aes_cipher(self.file_name,'password').encrypt_file()
-            
-        if self.radio_var.get()=='2':
-            two_fish(self.file_name,'password').encrypt_file()
-            print("Two Fish")
-       
-if __name__ == '__main__':
 
+        # if self.encrypt_button["text"]=="Encrypt":
+        if self.radio_var.get() == '1':
+            aes_cipher(self.original_file, 'password').encrypt_file()
+        if self.radio_var.get() == '2':
+            two_fish(self.original_file, 'password').encrypt_file()
+
+            # if self.decrypt_button["text"]=="Decrypt":
+        # print(self.decrypt_button["text"])
+        if self.radio_var_two.get() == '3':
+            aes_cipher(self.encrypted_file, 'password').decrypt_file()
+        if self.radio_var_two.get() == '4':
+            two_fish(self.encrypted_file, 'password').decrypt_file()
+
+    def change_to_encrypt(self):
+        self.encrypt_frame.pack(fill="both", expand=True)
+        self.decrypt_frame.forget()
+
+    def change_to_decrypt(self):
+        self.decrypt_frame.pack(fill="both", expand=True)
+        self.encrypt_frame.forget()
+        self.frame_var = self.frames[1]
+
+        print("Hi")
+
+
+if __name__ == '__main__':
     app = KrypterApp()
     app.mainloop()
