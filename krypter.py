312a1cdd7bbed34b8f983ee219b4831371e2dfb7
Joel Odhiambo <wesleyjoel4@gmail.com>
2022-08-26 22:49:05 +0300

    Optimized Code

--- krypter.py
+++ krypter.py
@@ -1,14 +1,16 @@
 import os
 import shutil
-from functools import partial
-import tkinter as tk
-from tkinter import DISABLED, Frame, Label, Menu, PhotoImage, StringVar, ttk
-from tkinter import filedialog
-
 import random
 import string
-
+import time
+import tkinter as tk
+import threading
 import ciphers.twofish_cipher
+
+from functools import partial
+from tkinter import DISABLED, Frame, Label, PhotoImage, StringVar, ttk
+from tkinter import filedialog
+
 from ciphers.aes_cipher import AesCipher as aes_cipher
 from ciphers.twofish_cipher import TwofishCipher as two_fish
 
@@ -19,18 +21,20 @@ class KrypterApp(tk.Tk, ciphers.twofish_cipher.TwofishCipher, ciphers.aes_cipher
     def __init__(self):
         super().__init__()
 
+        # Krypter directory structure
         self.app_dir = 'C:/Krypter/Files'
 
-        list = ('original files', 'encrypted files/AES',
-                'encrypted files/Twofish')
+        sub_directory_tree = ('original files', 'encrypted files/AES',
+                              'encrypted files/Twofish')
 
         app_path = partial(os.path.join, self.app_dir)
         make_directory = partial(os.makedirs, exist_ok=True)
 
-        for directories in map(app_path, list):
+        for directories in map(app_path, sub_directory_tree):
             make_directory(directories)
 
         self.title('Krypter')
+        # GUI icon
         self.tk.call('wm', 'iconphoto', self._w,
                      tk.PhotoImage(file='icons\Krypter Icon.png'))
         self.geometry('500x500')
@@ -40,55 +44,56 @@ class KrypterApp(tk.Tk, ciphers.twofish_cipher.TwofishCipher, ciphers.aes_cipher
         self.columnconfigure(1, weight=3)
 
         # self.menu_bar = Menu(self)
-
+        #
         # self.config(menu=self.menu_bar)
-
+        #
         # self.file_menu = Menu(self.menu_bar, tearoff=0)
         # self.file_menu.add_command(
         #     label="Open",
         #     command=self.open_file
         # )
-
+        #
         # self.file_menu.add_separator()
-
+        #
         # self.sub_menu = Menu(self.file_menu, tearoff=0)
-
+        #
         # self.sub_menu.add_command(label='Toggle Dark Mode')
         # self.file_menu.add_cascade(
         #     label="Preferences",
         #     menu=self.sub_menu
         # )
-
+        #
         # self.file_menu.add_separator()
-
+        #
         # self.file_menu.add_command(
         #     label="Quit",
         #     command=self.destroy
         # )
-
+        #
         # self.menu_bar.add_cascade(
         #     label="File",
         #     menu=self.file_menu,
         #     underline=0
         # )
-
+        #
         # self.encrypt_menu = Menu(self.menu_bar, tearoff=0)
-
+        #
         # self.menu_bar.add_cascade(
         #     label="Encrypt",
         #     menu=self.encrypt_menu,
         #     command=self.change_to_encrypt
-
+        #
         # )
-
+        #
         # self.decrypt_menu = Menu(self.menu_bar, tearoff=0)
-
+        #
         # self.menu_bar.add_cascade(
         #     label="Decrypt",
         #     menu=self.decrypt_menu,
         #     command=self.change_to_decrypt
         # )
 
+        # Create a notebook to display the Encryption and Decryption Frames
         self.notebook = ttk.Notebook(self)
 
         self.encrypt_frame = Frame(self.notebook)
@@ -100,13 +105,18 @@ class KrypterApp(tk.Tk, ciphers.twofish_cipher.TwofishCipher, ciphers.aes_cipher
         self.notebook.add(self.encrypt_frame, text="Encrypt")
         self.notebook.add(self.decrypt_frame, text="Decrypt")
 
+        """
+        Encryption Frame
+        # Here we build a frame that will be part of the GUI where the user can encrypt files
+        """
+
         self.label_choose = Label(self.encrypt_frame, text="1. Choose file to encrypt")
         self.label_choose.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
 
-        # File to encrypt Path Variable
+        # Path Variable for original file
         self.original_file_path = StringVar(None)
 
-        self.original_file_entry = ttk.Entry(self.encrypt_frame, width=44)
+        self.original_file_entry = ttk.Entry(self.encrypt_frame, width=44, state='disabled')
         self.original_file_entry.grid(column=0, row=1, sticky=tk.W, padx=20, pady=5)
 
         self.bool_one = StringVar(None)
@@ -119,36 +129,44 @@ class KrypterApp(tk.Tk, ciphers.twofish_cipher.TwofishCipher, ciphers.aes_cipher
         self.label_two.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
 
         self.default_password_var = tk.StringVar(None)
-
-        self.password_entry = ttk.Entry(
+        self.default_password_var.set("password")
+        self.password_entry_one = ttk.Entry(
             self.encrypt_frame, textvariable=self.default_password_var, width=44)
-        self.password_entry.grid(column=0, row=3, sticky=tk.W, padx=20, pady=5)
+        self.password_entry_one.grid(column=0, row=3, sticky=tk.W, padx=20, pady=5)
 
         self.generate_button = ttk.Button(self.encrypt_frame,
-                                          text="Generate Password", command=self.password_generator)
+                                          text="Generate Password", command=self.generate_password)
         self.generate_button.grid(column=1, row=3, sticky=tk.W, pady=5)
 
-        self.check_button = ttk.Checkbutton(
+        self.check_button_one = ttk.Checkbutton(
             self.encrypt_frame, text="Hide Password", onvalue=False, offvalue=True, command=self.toggle_password)
 
-        self.check_button.grid(column=0, row=4, sticky=tk.W, padx=100)
+        self.check_button_one.grid(column=0, row=4, sticky=tk.W, padx=100)
 
+        # Radiobutton variable ('self.radio_var' below and 'self.radio_var_two' in Decryption Frame) to store the
+        # state of radiobuttons '0' for None are active_radio_button '1' for AES Encryption '2' for Twofish Encryption '3' for AES
+        # Decryption '4' for Twofish Decryption.
         self.radio_var = StringVar(None)
+        self.radio_var.set('0')
+
         self.aes_radio_button_e = ttk.Radiobutton(self.encrypt_frame,
                                                   text='AES',
                                                   variable=self.radio_var,
-                                                  value=1)
+                                                  value=1,
+                                                  command=self.radio_button_state)
         self.aes_radio_button_e.grid(column=0, row=5, sticky=tk.W, padx=19)
         self.two_fish_radio_button_e = ttk.Radiobutton(self.encrypt_frame,
                                                        text='Twofish',
                                                        variable=self.radio_var,
-                                                       value=2)
+                                                       value=2,
+                                                       command=self.radio_button_state)
         self.two_fish_radio_button_e.grid(column=0, row=6, sticky=tk.W, padx=19)
 
         self.label_three = Label(
             self.encrypt_frame, text="3. Encryption")
         self.label_three.grid(column=0, row=7, sticky=tk.W, padx=5, pady=10)
 
+        # Path variable for encrypted files
         self.encrypted_file_dir_var = StringVar(None)
 
         self.label_four = Label(
@@ -168,19 +186,18 @@ class KrypterApp(tk.Tk, ciphers.twofish_cipher.TwofishCipher, ciphers.aes_cipher
         self.encryption_photo_image = self.encryption_photo.subsample(3, 3)
 
         self.encrypt_button = ttk.Button(self.encrypt_frame,
-                                         text="Encrypt", command=self.selected)
+                                         text="Encrypt", command=self.run_thread, state='disabled')
         self.encrypt_button.grid(column=0, row=9, sticky=tk.W, padx=19, pady=5)
 
-        # self.separator_two = ttk.Separator(self, orient='horizontal')
-        # self.separator_two.place(relx=0, rely=0.95, relheight=0.5, relwidth=1)
-
-        # self.progress_bar=ttk.Progressbar(self,orient='horizontal',mode='indeterminate',length=100)
-        # self.progress_bar.place(x=1,y=453)
+        """
+        Decryption Frame
+        # Here we build a frame that will be part of the GUI where the user can decrypt files
+        """
 
         self.label_choose_d = Label(self.decrypt_frame, text="1. Choose file to decrypt")
         self.label_choose_d.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
 
-        # File to decrypt path variable
+        # Path variable of the encrypted file
         self.encrypted_file_path = StringVar(None)
 
         self.encrypted_file_entry = ttk.Entry(
@@ -196,27 +213,29 @@ class KrypterApp(tk.Tk, ciphers.twofish_cipher.TwofishCipher, ciphers.aes_cipher
             self.decrypt_frame, text="2. Enter your password and choose a Decryption Algorithm")
         self.label_two.grid(column=0, row=2, sticky=tk.W, padx=5, pady=5)
 
-        self.default_password_var = tk.StringVar(None)
+        self.password_entry_two = ttk.Entry(
+            self.decrypt_frame, width=44)
+        self.password_entry_two.grid(column=0, row=3, sticky=tk.W, padx=20, pady=5)
 
-        self.password_entry = ttk.Entry(
-            self.decrypt_frame, textvariable=self.default_password_var, width=44)
-        self.password_entry.grid(column=0, row=3, sticky=tk.W, padx=20, pady=5)
-
-        self.check_button = ttk.Checkbutton(
+        self.check_button_two = ttk.Checkbutton(
             self.decrypt_frame, text="Hide Password", onvalue=False, offvalue=True, command=self.toggle_password)
 
-        self.check_button.grid(column=0, row=4, sticky=tk.W, padx=100)
+        self.check_button_two.grid(column=0, row=4, sticky=tk.W, padx=100)
 
         self.radio_var_two = StringVar(None)
+        self.radio_var_two.set('0')
+
         self.aes_radio_button_d = ttk.Radiobutton(self.decrypt_frame,
                                                   text='AES',
                                                   variable=self.radio_var_two,
-                                                  value=3)
+                                                  value=3,
+                                                  command=self.radio_button_state)
         self.aes_radio_button_d.grid(column=0, row=5, sticky=tk.W, padx=19)
         self.two_fish_radio_button_d = ttk.Radiobutton(self.decrypt_frame,
                                                        text='Twofish',
                                                        variable=self.radio_var_two,
-                                                       value=4)
+                                                       value=4,
+                                                       command=self.radio_button_state)
         self.two_fish_radio_button_d.grid(column=0, row=6, sticky=tk.W, padx=19)
 
         self.label_three = Label(
@@ -229,6 +248,7 @@ class KrypterApp(tk.Tk, ciphers.twofish_cipher.TwofishCipher, ciphers.aes_cipher
             self.decrypt_frame, text="Working Directory: ")
         self.label_five.grid(column=0, row=8, sticky=tk.W, padx=17, pady=5)
 
+        # Path variable for decrypted files
         self.decrypted_file_dir_var = StringVar(None)
 
         self.decrypted_file_path = ttk.Entry(
@@ -242,8 +262,17 @@ class KrypterApp(tk.Tk, ciphers.twofish_cipher.TwofishCipher, ciphers.aes_cipher
         self.encryption_photo_image = self.encryption_photo.subsample(3, 3)
 
         self.decrypt_button = ttk.Button(self.decrypt_frame,
-                                         text="Decrypt", command=self.selected)
+                                         text="Decrypt", state='disabled', command=self.run_thread)
         self.decrypt_button.grid(column=0, row=9, sticky=tk.W, padx=19, pady=5)
+
+        self.status_bar = ttk.Label(self, text=" Waiting... ", anchor=tk.W)
+        self.status_bar.pack(side=tk.BOTTOM, fill=tk.X)
+
+        self.progress_bar = ttk.Progressbar(self.status_bar, orient='horizontal', mode='indeterminate', length=100)
+        self.progress_bar.grid(column=3, row=3, columnspan=2, padx=390, pady=4)
+
+        # self.progress_bar_text = ttk.Label(self, text="Waiting", background='')
+        # self.progress_bar_text.place(x=329, y=475)
 
     def open_original_file(self):
 
@@ -259,12 +288,7 @@ class KrypterApp(tk.Tk, ciphers.twofish_cipher.TwofishCipher, ciphers.aes_cipher
         self.file_name = 'C:\Krypter\Files\original files\\' + \
                          os.path.basename(self.original_file)
         self.original_file_var.set(self.file_name)
-        # self.encrypted_file_dest.config(textvariable=self.original_file_var)
-        # if self.choose_button_one._name=="btn_one":
-        #     self.original_file_var.set(self.file_name)
-        # if self.choose_button_two._name=="btn_two":    
-        #     self.encrypted_file_var.set(self.file_name)
-        # print(self.file_name)
+
 
     def open_encrypted_file(self):
         self.file_types = (("All files", "*.*"), ("Text files", "*.txt*"))
@@ -274,17 +298,22 @@ class KrypterApp(tk.Tk, ciphers.twofish_cipher.TwofishCipher, ciphers.aes_cipher
         self.encrypted_file_var.set(self.encrypted_file)
         self.encrypted_file_entry.config(textvariable=self.encrypted_file_var)
 
-        # shutil.copy(self.encrypted_file, 'C:\Krypter\Files\original files')
-        # self.file_name = 'C:\Krypter\Files\original files\\' + \
-        #                  os.path.basename(self.encrypted_file)
-
     def toggle_password(self):
-        if self.password_entry.cget('show') == '':
-            self.password_entry.config(show='*')
+        if self.password_entry_one.cget('show') == '':
+            self.password_entry_one.config(show='*')
+            self.check_button_one.config(text="Show Password")
         else:
-            self.password_entry.config(show='')
+            self.password_entry_one.config(show='')
+            self.check_button_one.config(text="Hide Password")
 
-    def password_generator(self):
+        if self.password_entry_two.cget('show') == '':
+            self.password_entry_two.config(show='*')
+            self.check_button_two.config(text="Show Password")
+        else:
+            self.password_entry_two.config(show='')
+            self.check_button_two.config(text="Hide Password")
+
+    def generate_password(self):
         self.lowercase_characters = string.ascii_lowercase
         self.uppercase_characters = string.ascii_uppercase
         self.digits = string.digits
@@ -299,29 +328,51 @@ class KrypterApp(tk.Tk, ciphers.twofish_cipher.TwofishCipher, ciphers.aes_cipher
         self.default_password = "".join(self.password_combo)
         self.default_password_var.set(self.default_password)
 
-    def selected(self):
+    def radio_button_state(self):
+        if self.aes_radio_button_e or self.two_fish_radio_button_e:
+            self.encrypt_button.config(state='enabled')
 
-        # if self.encrypt_button["text"]=="Encrypt":
+        if self.aes_radio_button_d or self.two_fish_radio_button_d:
+            self.decrypt_button.config(state='enabled')
+
+    def run_thread(self):
+        execute_thread = threading.Thread(target=self.active_radio_button)
+        execute_thread.start()
+
+    def active_radio_button(self):
+
+        self.progress_bar.start(10)
+
+        if self.radio_var.get() and self.radio_var_two.get() == 0:
+            print("Choose and algorithm")
+
         if self.radio_var.get() == '1':
+            self.status_bar.config(text="  AES encryption in progress...")
             self.encrypted_file_dir_var.set(self.aes_encrypted_path)
-            aes_cipher(self.original_file, 'password').encrypt_file()
+            aes_cipher(self.original_file, self.password_entry_one.get()).encrypt_file()
             self.encrypted_file_dest.config(textvariable=self.encrypted_file_dir_var)
 
         if self.radio_var.get() == '2':
+            self.status_bar.config(text="  Twofish encryption in progress...")
             self.encrypted_file_dir_var.set(self.tf_encrypted_path)
-            two_fish(self.original_file, 'password').encrypt_file()
+            two_fish(self.original_file, self.password_entry_one.get()).encrypt_file()
             self.encrypted_file_dest.config(textvariable=self.encrypted_file_dir_var)
 
-            # if self.decrypt_button["text"]=="Decrypt":
-        # print(self.decrypt_button["text"])
         if self.radio_var_two.get() == '3':
+            self.status_bar.config(text="  AES decryption in progress...")
             self.decrypted_file_dir_var.set(self.aes_decrypted_path)
-            aes_cipher(self.encrypted_file, 'password').decrypt_file()
+            aes_cipher(self.encrypted_file, self.password_entry_two.get()).decrypt_file()
             self.decrypted_file_path.config(textvariable=self.decrypted_file_dir_var)
         if self.radio_var_two.get() == '4':
+            self.status_bar.config(text="  Twofish decryption in progress...")
             self.decrypted_file_dir_var.set(self.tf_decrypted_path)
-            two_fish(self.encrypted_file, 'password').decrypt_file()
+            two_fish(self.encrypted_file, self.password_entry_two.get()).decrypt_file()
             self.decrypted_file_path.config(textvariable=self.decrypted_file_dir_var)
+
+        self.status_bar.config(text=" File Encrypted!")
+        time.sleep(1)
+        self.status_bar.config(text=" Waiting...")
+        self.progress_bar.stop()
 
     def change_to_encrypt(self):
         self.encrypt_frame.pack(fill="both", expand=True)
@@ -332,7 +383,6 @@ class KrypterApp(tk.Tk, ciphers.twofish_cipher.TwofishCipher, ciphers.aes_cipher
         self.encrypt_frame.forget()
         self.frame_var = self.frames[1]
 
-        print("Hi")
 
 
 if __name__ == '__main__':
